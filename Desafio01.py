f4751087fe93c6cf173234f8cdace12fc5f8d0b1
Mauricio Costa <malesupremacy91@hotmail.com>
2022-05-20 16:53:38 -0300

    Projetos Básicos com Python

---
+++ Desafio01.py
@@ -0,0 +1,3 @@
+nome=input('Qual o seu nome?')
+print('Olá',nome,'!','Seja muito bem vindo à Quarta Aula de Programação com o Guanabara!')
+
---
+++ desafio02.py
@@ -0,0 +1,8 @@
+dia=input('Que dia você nasceu?')
+
+mês=input ('Qual o mês do seu nascimento?')
+
+ano=input('Qual o ano do seu nascimento?')
+
+print('Olá, você nasceu no dia',dia,' do mês', mês,'do ano de',ano)
+
